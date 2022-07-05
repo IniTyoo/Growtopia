da3b9ff4496b74efff4ecfe128d8a2e4ba4abc9c
Tyooo <88655862+IniTyoo@users.noreply.github.com>
2022-07-05 21:47:32 +0700

    Update Lua_Api.h

--- Umbrella/Lua_Api.h
+++ Umbrella/Lua_Api.h
@@ -1,41 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <future>
-#include <thread>
-#include <chrono>
-#include "corefunc.h"
-#include "userfunc.h"
-#include "json.hpp"
-#include <string>
-#include <windows.h>
-#include <direct.h>
-#include <iomanip>
-#include <vector>
-#include <string>
-#include <stdlib.h>
-#include <TlHelp32.h>
-#include <tchar.h>.
-
-#include <cmath>
-#include <future>
-
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_dx9.h"
-#include "imgui/imgui_impl_win32.h"
-#include "imgui/TextEditor.h"
-#include "ItemList.h"
-
-extern "C" {
-#include "lua/lauxlib.h"
-#include "lua/lua.h"
-#include "lua/luaconf.h"
-#include "lua/lualib.h"
-}
-
-#include "multibot.h"
-#include "variable.h"
-
-
+#include "include.h"
 
 void execute_thread(lua_State* state, std::string text) {
 	luaL_dostring(state, text.c_str());
