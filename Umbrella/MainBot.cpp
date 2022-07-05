7a14d2be6815d599e95c2bfaf472259e08a43e5e
Tyooo <88655862+IniTyoo@users.noreply.github.com>
2022-07-05 21:39:34 +0700

    cb cb

--- Umbrella/MainBot.cpp
+++ Umbrella/MainBot.cpp
@@ -1,53 +1,8 @@
-#include <iostream>
-#include <fstream>
-#include <future>
-#include <thread>
-#include <chrono>
-#include "corefunc.h"
-#include "userfunc.h"
-#include "json.hpp"
-#include <string>
-#include <windows.h>
-#include <direct.h>
-#include <iomanip>
-#include <vector>
-#include <string>
-#include <stdlib.h>
-#include <TlHelp32.h>
-#include <tchar.h>.
-
-#include <cmath>
-#include <future>
-
-#include "imgui/imgui.h"
-#include "imgui/imgui_impl_dx9.h"
-#include "imgui/imgui_impl_win32.h"
-#include "imgui/TextEditor.h"
-#include "ItemList.h"
-
-extern "C" {
-#include "lua/lauxlib.h"
-#include "lua/lua.h"
-#include "lua/luaconf.h"
-#include "lua/lualib.h"
-}
-
 #include "Lua_Api.h"
 #include "multibot.h"
 #include "variable.h"
 
 
-#include <d3d9.h>
-#include <tchar.h>
-#include <iostream>
-#include "globals.h"
-#include "xorstr.hpp"
-#include "proton/variant.hpp"
-//#define CPPHTTPLIB_OPENSSL_SUPPORT
-//#include "httplib.h"
-
-#include "auth.hpp"
-#include "skStr.h"
 #define CURL_STATICLIB
 std::string tm_to_readable_time(tm ctx);
 static std::time_t string_to_timet(std::string timestamp);
