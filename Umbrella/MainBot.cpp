352a98db5ccf61987b4a3d26562f827ea2579876
Tyooo <88655862+IniTyoo@users.noreply.github.com>
2022-07-05 15:29:00 +0700

    rapihin file

--- Umbrella/MainBot.cpp
+++ Umbrella/MainBot.cpp
@@ -33,6 +33,7 @@ extern "C" {
 }
 
 #include "Lua_Api.h"
+#include "multibot.h"
 
 #include <d3d9.h>
 #include <tchar.h>
@@ -100,38 +101,6 @@ string word = "";
 string name = "";
 string last;
 
-vector<GrowtopiaBot> bots;
-GrowtopiaBot create(string username, string password) {
-    GrowtopiaBot bot = { username, password };
-    http::Request request{ "http://growtopia2.com/growtopia/server_data.php" };
-    const auto response = request.send("POST", "version=1&protocol=128", { "Content-Type: application/x-www-form-urlencoded" });
-    rtvar var = rtvar::parse({ response.body.begin(), response.body.end() });
-    var.serialize();
-    if (var.find("server")) {
-        bot.SERVER_HOST = var.get("server");
-        bot.SERVER_PORT = std::stoi(var.get("port"));
-    }
-    cout << "Parsing port and ip is done. port is " << to_string(bot.SERVER_PORT).c_str() << " and ip is " << bot.SERVER_HOST << endl;
-    bot.userInit();
-    bots.push_back(bot);
-    return bot;
-}
-
-static void autocollecting(int range)
-{
-    using namespace std::literals::chrono_literals;
-    for (auto& bot : bots)
-    {
-        while (bot.autocollect)
-        {
-            for (auto& bot : bots)
-            {
-                bot.Collect(bot.range);
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        }
-    }
-}
 
 std::string tm_to_readable_time(tm ctx) {
     char buffer[80];
@@ -340,26 +309,6 @@ inline bool exists_test(const string& name) {
     ifstream f(name.c_str());
     return f.good();
 }
-
-
-void spamthread(std::string text1)
-{
-    using namespace std::literals::chrono_literals;
-    for (auto& bot : bots)
-    {
-        while (bot.autospamm)
-        {
-            for (auto& bot : bots)
-            {
-                bot.SendPacket(2, "action|input\n|text|" + text1, bot.peer);
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(bot.intervalspam));
-        }
-    }
-}
-
-
-
 
 
 static void HelpMarker(const char* desc)
