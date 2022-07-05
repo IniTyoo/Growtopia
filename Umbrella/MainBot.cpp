a5abf32fa19c17794ce830d8a745cb966628470d
Tyooo <88655862+IniTyoo@users.noreply.github.com>
2022-07-06 05:44:37 +0700

    Update MainBot.cpp

--- Umbrella/MainBot.cpp
+++ Umbrella/MainBot.cpp
@@ -33,6 +33,8 @@ extern "C" {
 }
 
 
+#include "multibot.h"
+
 #include <d3d9.h>
 #include <tchar.h>
 #include <iostream>
@@ -62,75 +64,9 @@ api KeyAuthApp(namex, ownerid, secret, version, url, sslPin);
 using namespace std;
 using json = nlohmann::json;
 
-int active_tab = 0;
-static int range = 0;
 
-static bool selected[60] = { false };
-static bool open = true;
-static char usernamebot[120];
-static char passwordbot[120];
-static char usernamelogin[120];
-static char passwordlogin[120];
-static char license[120];
 
-static char worldName[60];
-char namaFile[40] = "Script.lua";
-static char growid[70];
 
-// Auto Spam
-static bool autospam;
-static char spamtext[500];
-float interval = 4;
-
-bool loginpacket = false;
-bool autocollect = false;
-bool autoreconnect = false;
-static int current_item;
-static bool selectall;
-
-// Main Tab
-bool set = true;
-static int speed = 1;
-bool follow = false;
-bool autoacc = false;
-bool asdhgsahdasvdsagsbdadhasgdbsajhdsauhdsajhdjashdjahsd = true;
-
-string word = "";
-string name = "";
-string last;
-
-vector<GrowtopiaBot> bots;
-GrowtopiaBot create(string username, string password) {
-    GrowtopiaBot bot = { username, password };
-    http::Request request{ "http://growtopia2.com/growtopia/server_data.php" };
-    const auto response = request.send("POST", "version=1&protocol=128", { "Content-Type: application/x-www-form-urlencoded" });
-    rtvar var = rtvar::parse({ response.body.begin(), response.body.end() });
-    var.serialize();
-    if (var.find("server")) {
-        bot.SERVER_HOST = var.get("server");
-        bot.SERVER_PORT = std::stoi(var.get("port"));
-    }
-    cout << "Parsing port and ip is done. port is " << to_string(bot.SERVER_PORT).c_str() << " and ip is " << bot.SERVER_HOST << endl;
-    bot.userInit();
-    bots.push_back(bot);
-    return bot;
-}
-
-static void autocollecting(int range)
-{
-    using namespace std::literals::chrono_literals;
-    for (auto& bot : bots)
-    {
-        while (bot.autocollect)
-        {
-            for (auto& bot : bots)
-            {
-                bot.Collect(bot.range);
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(50));
-        }
-    }
-}
 
 std::string tm_to_readable_time(tm ctx) {
     char buffer[80];
@@ -362,22 +298,6 @@ inline bool exists_test(const string& name) {
     return f.good();
 }
 
-
-void spamthread(std::string text1)
-{
-    using namespace std::literals::chrono_literals;
-    for (auto& bot : bots)
-    {
-        while (bot.autospamm)
-        {
-            for (auto& bot : bots)
-            {
-                bot.SendPacket(2, "action|input\n|text|" + text1, bot.peer);
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds(bot.intervalspam));
-        }
-    }
-}
 
 
 
