221ec7b912dcb1c95f5a5ae7867c6cdb6dea1b9d
Tyooo <88655862+IniTyoo@users.noreply.github.com>
2022-07-06 05:56:52 +0700

    Update MainBot.cpp

--- Umbrella/MainBot.cpp
+++ Umbrella/MainBot.cpp
@@ -35,6 +35,7 @@ extern "C" {
 
 #include "multibot.h"
 #include "Lua_Api.h"
+#include "variable.h"
 
 #include <d3d9.h>
 #include <tchar.h>
@@ -53,51 +54,11 @@ static std::time_t string_to_timet(std::string timestamp);
 static std::tm timet_to_tm(time_t timestamp);
 
 using namespace KeyAuth;
-
-std::string sslPin = "ssl pin key (optional)"; // don't change unless you intend to pin public certificate key. you can get here in the "Pin SHA256" field https://www.ssllabs.com/ssltest/analyze.html?d=keyauth.win&latest. If you do this you need to be aware of when SSL key expires so you can update it
-std::string namex = "payung"; // application name. right above the blurred text aka the secret on the licenses tab among other tabs
-std::string ownerid = "MCbJ0h8d3p"; // ownerid, found in account settings. click your profile picture on top right of dashboard and then account settings.
-std::string secret = "2e013a6ac7c57740f8ad0ecab08f7c7bf0af0d0aad7b5812e18fe082baf41b65"; // app secret, the blurred text on licenses tab and other tabs
-std::string version = "1.0"; // leave alone unless you've changed version on website
-std::string url = "https://umberella-server.000webhostapp.com/api/1.1/"; // change if you're self-hosting
-api KeyAuthApp(namex, ownerid, secret, version, url, sslPin);
-
 using namespace std;
 using json = nlohmann::json;
 
 
 bool activeglobal = true;
-
-
-std::string tm_to_readable_time(tm ctx) {
-    char buffer[80];
-
-    strftime(buffer, sizeof(buffer), "%a %m/%d/%y %H:%M:%S %Z", &ctx);
-
-    return std::string(buffer);
-}
-
-static std::time_t string_to_timet(std::string timestamp) {
-    auto cv = strtol(timestamp.c_str(), NULL, 10); // long
-
-    return (time_t)cv;
-}
-
-static std::tm timet_to_tm(time_t timestamp) {
-    std::tm context;
-
-    localtime_s(&context, &timestamp);
-
-    return context;
-}
-
-inline bool exists_test(const string& name) {
-    ifstream f(name.c_str());
-    return f.good();
-}
-
-
-
 
 
 
