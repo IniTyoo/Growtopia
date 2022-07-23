d466c0e64c838eb617591d6244c7e89ac0030c00
Aseppp <107647697+DrAseppp002@users.noreply.github.com>
2022-07-23 12:55:18 +0700

    Delete build.bat

--- build.bat
+++
@@ -1,9 +0,0 @@
-@REM Build for Visual Studio compiler. Run your copy of vcvars32.bat or vcvarsall.bat to setup command-line compiler.
-@set OUT_EXE=external
-@set INCLUDES=/Ivendor/enet/include /Ivendor/imgui /Ivendor/lua /I"%DXSDK_DIR%/Include"
-@set SOURCES=src/*.cpp src/misc/*.cpp src/wrapper/*.cpp vendor/enet/*c vendor/imgui/*.cpp vendor/lua/*.c
-@set LIBS=/LIBPATH:"%DXSDK_DIR%/Lib/x86" d3d9.lib winmm.lib ws2_32.lib
-mkdir Release
-mkdir obj
-
-cl /std:c++17 /GS /GL /analyze- /W3 /Gy /Zc:wchar_t /Zi /Gm- /O2 /Zc:inline /fp:precise /D _CONSOLE /D UNICODE /D _UNICODE /WX- /Zc:forScope /Gd /Oy- /Oi /EHsc /nologo /Foobj/ /FeRelease/%OUT_EXE%.exe %SOURCES% %INCLUDES% /link %LIBS%
